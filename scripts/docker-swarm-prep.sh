da6e797bfa332f2a47bddad5d8c2f149073d2b5e
postables <postables@rtradetechnologies.com>
2019-10-24 15:28:42 -0700

    scripts: cleanup swarm prep script

--- scripts/docker-swarm-prep.sh
+++ scripts/docker-swarm-prep.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
 # prepares a host for docker swarm usage by making access to the ports more restricted
+# explanation of port numbers
+# 2377/tcp (secure client to daemon communication)
+# 7946/tcp+udp (control plane gossip)
+# 4789/udp (VXLAN-based overlay networks)
 
 function error_handle() {
     if [[ "$?" != 0 ]]; then
@@ -8,7 +12,6 @@ function error_handle() {
         exit 1
     fi
 }
-
 
 HOSTS_FILE="$1"
 
@@ -27,23 +30,17 @@ if [[ "$ACTIVE" != "active" ]]; then
     error_handle
 fi
 
+# $1 of this is the first paramter passed in
+# to call: manager_ports_allow "192.168.1.2"
+function update_ports_allowed() {
+    sudo ufw allow insert 1 from "$1" to any port 2377 proto tcp
+    sudo ufw allow insert 1 from "$1" to any port 7946 proto tcp
+    sudo ufw allow insert 1 from "$1" to any port 7946 proto udp
+    sudo ufw allow insert 1 from "$1" to any port 4789 proto udp
+}
 
 echo "[INFO] updating ufw rules for docker-swarm communication"
 while IFS= read -r line; do
-    # specify who can connect
-    sudo ufw allow from "$line" to any port 2377 proto tcp
-    error_handle
-    sudo ufw allow from "$line" to any port 7946 # not specifying proto does both tcp+udp
-    error_handle
-    sudo ufw allow from "$line" to any port 4789 proto udp
-    error_handle
-    # specify who can't connect (everyone else)
-    sudo ufw deny from any to any port 2377 proto tcp
-    error_handle
-    sudo ufw deny from any to any port 7946
-    error_handle
-    sudo ufw deny from any to any port 4789 proto udp
-    error_handle
+    update_ports_allowed "$line"
 done < "$HOSTS_FILE"
-
 echo "[INFO] finished updating ufw rules for docker-swarm communication"
